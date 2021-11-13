b6019a0046e696bb2a3536e088f0b2cd648f23ac
aiperi09 <aiperi.miran@bk.ru>
2021-11-13 20:48:00 +0300

    commit

--- app/src/main/java/com/geektech/lesson1/MainActivity.java
+++ app/src/main/java/com/geektech/lesson1/MainActivity.java
@@ -16,25 +16,47 @@ import com.google.android.material.textfield.TextInputEditText;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextInputEditText Username, Password;
-    private Button GO;
+    private TextInputEditText username, password;
+    private Button go;
     private ImageView car;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onClick();
+        initView();
+        initGlide();
 
-        Username = findViewById(R.id.gg);
-        Password = findViewById(R.id.password);
-        GO = findViewById(R.id.go);
-        car = findViewById(R.id.car);
-        Log.d("oooo", "onCreate");
+    }
 
+    private void onClick() {
+        go.setOnClickListener(v -> {
+           if (password.getText().toString().length()>=6 &&username.getText().toString().length()>=1){
+
+           } else if (password.getText().toString().isEmpty() && username.getText().toString().isEmpty()) {
+               username.setError(getString(R.string.erro_login));
+               password.setError(getString(R.string.error_password));
+           }else if(password.getText().toString().length()<6){
+               password.setError(getString(R.string.error_password6));
+           }else if(username.getText().toString().length()<1){
+               username.setError(getString(R.string.error_user));
+
+           }
+
+        });
+    }
+
+    private void initGlide() {
         String URI = "https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg";
         Glide.with(MainActivity.this).load(URI).into(car);
+    }
+
+    private void initView() {
+        username = findViewById(R.id.gg);
+        password = findViewById(R.id.password);
+        go = findViewById(R.id.go);
+        car = findViewById(R.id.car);
     }
 
 }
