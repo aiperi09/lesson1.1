bd779c02457818f8e4685bbcaced655d62879b5f
aiperi09 <aiperi.miran@bk.ru>
2021-11-15 20:40:35 +0300

    commit

--- app/src/main/java/com/geektech/lesson1/Activity2.java
+++ app/src/main/java/com/geektech/lesson1/Activity2.java
@@ -1,14 +1,72 @@
 package com.geektech.lesson1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class Activity2 extends AppCompatActivity {
+
+    private static final int GALLERY = 123;
+    private static final int PHOTO = 731;
+    private EditText name;
+    private EditText password;
+    private ImageView IV_Photo;
+    private TextView open_galery;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        initView();
+        getInformation();
+
+        open_galery.setOnClickListener(v ->{
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_PICK);
+            startActivityForResult(intent, PHOTO);
+        });
+
+        IV_Photo.setOnClickListener(v ->{
+            Intent intent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,GALLERY);
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY && resultCode == RESULT_OK && data != null) {
+            Glide.with(this).load(data.getData().toString()).circleCrop().into(IV_Photo);
+        }
+        if (requestCode == PHOTO && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Glide.with(this).load(imageBitmap).circleCrop().into(IV_Photo);
+
+        }
+    }
+    private void getInformation() {
+        String name = getIntent().getStringExtra("key1");
+        this.name.setText(name);
+        String password = getIntent().getStringExtra("key2");
+        this.password.setText(password);
+    }
+
+    private void initView() {
+        name = findViewById(R.id.gmail_com);
+        password = findViewById(R.id.parol);
+        IV_Photo = findViewById(R.id.edit_foto);
+        open_galery = findViewById((R.id.edit));
     }
 }
--- app/src/main/java/com/geektech/lesson1/MainActivity.java
+++ app/src/main/java/com/geektech/lesson1/MainActivity.java
@@ -4,11 +4,14 @@ package com.geektech.lesson1;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
@@ -24,27 +27,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onClick();
         initView();
         initGlide();
-
+        go.setOnClickListener(v -> onClick());
     }
 
     private void onClick() {
-        go.setOnClickListener(v -> {
-           if (password.getText().toString().length()>=6 &&username.getText().toString().length()>=1){
-
-           } else if (password.getText().toString().isEmpty() && username.getText().toString().isEmpty()) {
-               username.setError(getString(R.string.erro_login));
-               password.setError(getString(R.string.error_password));
-           }else if(password.getText().toString().length()<6){
-               password.setError(getString(R.string.error_password6));
-           }else if(username.getText().toString().length()<1){
-               username.setError(getString(R.string.error_user));
-
-           }
-
-        });
+                if (password.getText().toString().length()>=6 &&username.getText().toString().length()>=1){
+                    Intent intent = new Intent(MainActivity.this,Activity2.class);
+                    String name = username.getText().toString();
+                    intent.putExtra("key1",name);
+                    intent.putExtra("key2",password.getText().toString());
+                    startActivity(intent);
+                } else if (password.getText().toString().isEmpty() && username.getText().toString().isEmpty()) {
+                    username.setError(getString(R.string.erro_login));
+                    password.setError(getString(R.string.error_password));
+                }else if(password.getText().toString().length()<6){
+                    password.setError(getString(R.string.error_password6));
+                }else if(username.getText().toString().length()<1) {
+                    username.setError(getString(R.string.error_user));
+                }
     }
 
     private void initGlide() {
@@ -71,3 +73,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+ 
